cbc8523b62b4c52a53c69838028dacd70176124c
Andrey Burmenko <a_burmenko@wargaming.net>
2024-07-01 00:00:31 +0300

    replace deprecated metod. fix warning: URI.escape is obsolete

--- lib/puppet/functions/hiera_http.rb
+++ lib/puppet/functions/hiera_http.rb
@@ -110,8 +110,8 @@ Puppet::Functions.create_function(:hiera_http) do
 
 
   def http_get(context, options)
-    uri = URI.parse(URI.escape(options['uri']))
-    host, port, path = uri.host, uri.port, URI.escape(context.interpolate(uri.request_uri))
+    uri = URI.parse(URI::DEFAULT_PARSER.escape(options['uri']))
+    host, port, path = uri.host, uri.port, URI::DEFAULT_PARSER.escape(context.interpolate(uri.request_uri))
 
     if context.cache_has_key(path)
       context.explain { "Returning cached value for #{path}" }
